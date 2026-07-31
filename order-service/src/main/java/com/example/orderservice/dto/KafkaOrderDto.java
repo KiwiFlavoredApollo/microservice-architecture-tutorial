3ac37d7d706e2b06600967e32127ad50d153f064
fractalbarista <fractalbarista@gmail.com>
2026-07-31 11:25:55 +0900

    KafkaOrderDto 클래스 추가 OrderProducerService#sendForSinkConnect() 메소드 추가

--- order-service/src/main/java/com/example/orderservice/dto/KafkaOrderDto.java
+++ order-service/src/main/java/com/example/orderservice/dto/KafkaOrderDto.java
@@ -1,13 +1,43 @@
 package com.example.orderservice.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 public class KafkaOrderDto implements Serializable {
     private Schema schema;
     private Payload payload;
+
+    @Data
+    @Builder
+    public static class Schema {
+        private String type;
+        private List<Field> fields;
+        private boolean optional;
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class Field {
+        private String type;
+        private boolean optional;
+        private String field;
+    }
+
+    @Data
+    @Builder
+    public static class Payload {
+        private String order_id;
+        private String user_id;
+        private String product_id;
+        private Integer qty;
+        private Integer unit_price;
+        private Integer total_price;
+    }
 }
--- order-service/src/main/java/com/example/orderservice/kafka/OrderProducerService.java
+++ order-service/src/main/java/com/example/orderservice/kafka/OrderProducerService.java
@@ -1,6 +1,7 @@
 package com.example.orderservice.kafka;
 
-import com.example.orderservice.dto.*;
+import com.example.orderservice.dto.KafkaOrderDto;
+import com.example.orderservice.dto.OrderDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -8,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -16,15 +16,18 @@ import java.util.List;
 public class OrderProducerService {
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    List<Field> fields = Arrays.asList(new Field("string", true, "order_id"),
-            new Field("string", true, "user_id"),
-            new Field("string", true, "product_id"),
-            new Field("int32", true, "qty"),
-            new Field("int32", true, "total_price"),
-            new Field("int32", true, "unit_price"));
-    Schema schema = Schema.builder()
+    private static final List<KafkaOrderDto.Field> FIELDS = List.of(
+            new KafkaOrderDto.Field("string", true, "order_id"),
+            new KafkaOrderDto.Field("string", true, "user_id"),
+            new KafkaOrderDto.Field("string", true, "product_id"),
+            new KafkaOrderDto.Field("int32", true, "qty"),
+            new KafkaOrderDto.Field("int32", true, "total_price"),
+            new KafkaOrderDto.Field("int32", true, "unit_price")
+    );
+
+    private static final KafkaOrderDto.Schema SCHEMA = KafkaOrderDto.Schema.builder()
             .type("struct")
-            .fields(fields)
+            .fields(FIELDS)
             .optional(false)
             .name("orders")
             .build();
@@ -39,7 +42,7 @@ public class OrderProducerService {
         String jsonInString = "";
         try {
             jsonInString = mapper.writeValueAsString(orderDto);
-        } catch(JsonProcessingException ex) {
+        } catch (JsonProcessingException ex) {
             ex.printStackTrace();
         }
 
@@ -49,23 +52,23 @@ public class OrderProducerService {
         return orderDto;
     }
 
-    public OrderDto send4SinkConnect(String topic, OrderDto orderDto) {
-        Payload payload = Payload.builder()
-                .order_id(orderDto.getOrderId())
-                .user_id(orderDto.getUserId())
-                .product_id(orderDto.getProductId())
-                .qty(orderDto.getQty())
-                .total_price(orderDto.getTotalPrice())
-                .unit_price(orderDto.getUnitPrice())
-                .build();
+    public OrderDto sendForSinkConnect(String topic, OrderDto orderDto) {
+        KafkaOrderDto.Payload.PayloadBuilder builder = KafkaOrderDto.Payload.builder();
+        builder.order_id(orderDto.getOrderId());
+        builder.user_id(orderDto.getUserId());
+        builder.product_id(orderDto.getProductId());
+        builder.qty(orderDto.getQty());
+        builder.unit_price(orderDto.getUnitPrice());
+        builder.total_price(orderDto.getTotalPrice());
+        KafkaOrderDto.Payload payload = builder.build();
 
-        KafkaOrderDto kafkaOrderDto = new KafkaOrderDto(schema, payload);
+        KafkaOrderDto kafkaOrderDto = new KafkaOrderDto(SCHEMA, payload);
 
         ObjectMapper mapper = new ObjectMapper();
         String jsonInString = "";
         try {
             jsonInString = mapper.writeValueAsString(kafkaOrderDto);
-        } catch(JsonProcessingException ex) {
+        } catch (JsonProcessingException ex) {
             ex.printStackTrace();
         }
 
