f396358a548c40985ef02dbae7415e27531d0fb4
fractalbarista <fractalbarista@gmail.com>
2026-08-07 15:37:25 +0900

    우선순위를 조정하여 RequestLoggingFilter 다시 추가 불필요한 주석 제거

---
+++ user-service/src/main/java/com/example/userservice/security/RequestLoggingFilter.java
@@ -0,0 +1,32 @@
+package com.example.userservice.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Slf4j
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        log.debug("{} {} from {}", request.getMethod(), request.getRequestURI(), request.getRemoteAddr());
+
+        filterChain.doFilter(request, response);
+    }
+
+}
